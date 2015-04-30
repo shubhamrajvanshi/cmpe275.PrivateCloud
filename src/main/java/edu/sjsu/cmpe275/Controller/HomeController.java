5fb44784dbd347abd141e6d7d9510b380abc795a
shubhamrajvanshi <shubhamrajvanshi9@gmail.com>
2015-04-29 17:50:28 -0700

    Added controller functions

--- src/main/java/edu/sjsu/cmpe275/Controller/HomeController.java
+++ src/main/java/edu/sjsu/cmpe275/Controller/HomeController.java
@@ -11,12 +11,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.sjsu.cmpe275.VmService.VmService;
 
 /**
  * Handles requests for the application home page.
  */
+/**
+ * @author Shubham
+ *
+ */
+
 @Controller
 public class HomeController {
 	
@@ -45,9 +51,108 @@ public class HomeController {
 	//	vmServiceImpl.createVM(7,"vm3");
 	//	vmServiceImpl.powerOn("CentOS_1024MB_2cpu_tmpl1");
 	//	vmServiceImpl.powerOFF("CentOS_1024MB_2cpu_tmpl1");
-		vmServiceImpl.addHost("130.65.133.122", "root", "12!@qwQW");
+	//	vmServiceImpl.addHost("130.65.133.122", "root", "12!@qwQW");
 		
 		return "home";
 	}
 	
+	/**
+	 * Start user login and logout functions
+	 */
+	
+	@RequestMapping(value = "/userlogin", method = RequestMethod.GET)
+	public String login(@RequestParam(value="username") String username,
+			@RequestParam(value="password") String password) {
+		logger.info("Logging user ", username);
+		
+		//Implement user session to login a user
+		
+	
+
+		return "User Logged In";
+	}
+	
+	@RequestMapping(value = "/userlogout", method = RequestMethod.GET)
+	public String logout(@RequestParam(value="username") String username
+			) {
+		logger.info("Logging out user ", username);
+		
+		//Implement user session to login a user
+		
+	
+
+		return "User logged out";
+	}
+	
+	/**
+	 * Start of vm and host controller functions
+	 */
+	
+	
+	/**
+	 * @param templateid
+	 * @param vmname
+	 * @return 
+	 */
+	@RequestMapping(value = "/createvm", method = RequestMethod.POST)
+	public String createvm(@RequestParam(value="templateid") int templateid,
+						   @RequestParam(value="vmname") String vmname) {
+		logger.info("Creating VM with templateid ", templateid);
+		
+		
+		vmServiceImpl.createVM(templateid,vmname);
+	
+
+		return "home";
+	}
+		
+	/**
+	 * @param vmname
+	 * @return
+	 */
+	@RequestMapping(value = "/poweroff", method = RequestMethod.PUT)
+	public String poweroff(@RequestParam(value="vmname") String vmname) {
+		logger.info("Powering Off vm ", vmname);
+		
+		
+		vmServiceImpl.powerOFF(vmname);
+	
+
+		return "home";
+	}
+	
+	/**
+	 * @param vmname
+	 * @return
+	 */
+	@RequestMapping(value = "/poweron", method = RequestMethod.PUT)
+	public String poweron(@RequestParam(value="vmname") String vmname) {
+		logger.info("Powering On vm ", vmname);
+		
+		
+		vmServiceImpl.powerOn(vmname);
+	
+
+		return "home";
+	}
+	
+	/**
+	 * @param hostname
+	 * @param user
+	 * @param password
+	 * @return
+	 */
+	@RequestMapping(value = "/addhost", method = RequestMethod.POST)
+	 public String addhost(@RequestParam(value="hostname") String hostname,@RequestParam(value="user") String user,
+			 			   @RequestParam(value="password") String password)	{
+
+		
+		vmServiceImpl.addHost(hostname, user, password);
+		
+		
+		return null;
+		}
+	
+	
+//Closing of Controller	
 }
