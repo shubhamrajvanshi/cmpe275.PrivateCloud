85508ca63b235673c24ff3d8f721b27879b90742
Priyanka Karpe <priyankakarpe12@yahoo.com>
2015-05-02 23:59:12 -0700

    Adding models

---
+++ src/main/java/edu/sjsu/cmpe275/VmModel/User.java
@@ -0,0 +1,83 @@
+package edu.sjsu.cmpe275.VmModel;
+
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.mkyong.users.model.UserRole;
+ 
+@Entity
+@Table(name = "user", catalog = "private_cloud")
+public class User {
+	
+	String email;
+	String firstname;
+	String lastname;
+	String password;
+	boolean isadmin;
+	private Set<VMDetails> vmDetails = new HashSet<VMDetails>(0);
+	
+	public User() {
+	}
+ 
+	public User(Integer id, String username, String password, boolean isadmin) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.isadmin = isadmin;
+	}
+ 
+	@Id
+	@Column(name = "id", unique = true, 
+		nullable = false)
+	public String getId() {
+		return this.id;
+	}
+ 
+	public void setId(String id) {
+		this.id = id;
+	}
+ 
+	@Column(name = "username", 
+			nullable = false, length = 20)
+	public String getUsername() {
+		return this.username;
+	}
+	 
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	@Column(name = "password", 
+		nullable = false, length = 20)
+	public String getPassword() {
+		return this.password;
+	}
+ 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+ 
+	@Column(name = "isadmin", nullable = false)
+	public boolean isadmin() {
+		return this.isadmin;
+	}
+ 
+	public void setisadmin(boolean isadmin) {
+		this.isadmin = isadmin;
+	}
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+	public Set<VMDetails> getVmdetails() {
+		return this.vmDetails;
+	}
+ 
+	public void setVmdetails(Set<VMDetails> vmDetails) {
+		this.vmDetails = vmDetails;
+	}
+}
---
+++ src/main/java/edu/sjsu/cmpe275/VmModel/VMDetails.java
@@ -0,0 +1,74 @@
+package edu.sjsu.cmpe275.VmModel;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.mkyong.users.model.User;
+
+@Entity
+@Table(name = "vmdetails", catalog = "private_cloud", uniqueConstraints = @UniqueConstraint(columnNames = { "vmid", "userid" }))
+public class VMDetails {
+
+	private Integer vmid;
+	private User user;
+	private String vmname;
+	private Integer vmstate;
+
+	public VMDetails() {
+	}
+
+	public VMDetails(User user, Integer vmid, String vmname, Integer vmstate) {
+		this.user = user;
+		this.vmid = vmid;
+		this.vmname = vmname;
+		this.vmstate = vmstate;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "vmid", unique = true, nullable = false)
+	public Integer getVmid() {
+		return this.vmid;
+	}
+
+	public void setVmid(Integer vmid) {
+		this.vmid = vmid;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id", nullable = false)
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@Column(name = "vmname", nullable = false, length = 50)
+	public String getVmname() {
+		return this.vmname;
+	}
+
+	public void setVmname(String vmname) {
+		this.vmname = vmname;
+	}
+	
+	@Column(name = "vmstate", nullable = false)
+	public String getVmstate() {
+		return this.vmstate;
+	}
+
+	public void setVmstate(String vmstate) {
+		this.vmstate = vmstate;
+	}
+}
